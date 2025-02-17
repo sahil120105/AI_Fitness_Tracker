8d06c6cd4428395e6100cebb9ee925dcddc4b732
Sahil Ranadive <sahilranadive12@gmail.com>
2025-02-18 00:52:28 +0530

    Setup done

---
+++ src/data/make_dataset.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+from glob import glob
+
+# ----------------------------------------------------
+# Read Single CSV
+# ----------------------------------------------------
+
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
+
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
